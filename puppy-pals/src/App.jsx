5aea7abbf6f2b25c2a75b44f10996b962856afba
Hutch-IT <joshua.hutcherson1@gmail.com>
2024-02-11 14:36:55 -0500

    final

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -4,15 +4,30 @@ import {puppyList} from './data.js'
 
 function App() {
     const [puppies, setPuppies] = useState(puppyList);
-    console.log(puppies);
+    const [featPupId, setFeatPupId] = useState(null);
+    const featuredPup = puppies.find((pup) => pup.id === featPupId)
+    console.log(featuredPup);
     return (
         <>
+            {featPupId && (
+                <div className="puppies">
+                    <h2>{featuredPup.name}</h2>
+                    <ul>
+                        <li>Age: {featuredPup.age}</li>
+                        <li>Email: {featuredPup.email}</li>
+                    </ul>
+                </div>
+            )}
             <div>
                 {
                     puppies.map((puppy) => {
-                        return <p key={puppy.id}>{puppy.name}</p>
+                        return <p onClick={() => {
+                            setFeatPupId(puppy.id)
+                        }} key={puppy.id}>{puppy.name}</p>
                     })
                 }
+
+
             </div>
 
         </>
